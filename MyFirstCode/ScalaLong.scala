ab58e495aeaf63918a4de5d4495374f3ba292065
Morries <94471415+morris4117@users.noreply.github.com>
2023-03-07 16:01:12 +0530

    Add files via upload

---
+++ MyFirstCode/ScalaLong.scala
@@ -0,0 +1,13 @@
+object ScalaExamples{
+   def main(args:Array[String])={
+   
+   var i:Short = 32755
+   println(i)
+
+   var j:Int = 327554678
+   println(j)
+
+   var k:Long = 32757850498475l
+   println(k)
+   }
+ }
---
+++ MyFirstCode/ScalaShort.scala
@@ -0,0 +1,8 @@
+object ScalaExamples{
+   def main(args:Array[String])={
+   
+   var i:Short = 32755
+   i = 36777
+   println(i)
+   }
+ }
